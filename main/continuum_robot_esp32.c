f7dfab0fbedf4be7f22a8222620531f19e3eaeeb
2364701242 <2364701242@qq.com>
2024-09-25 15:21:44 +0800

    搭建了FreeRTOS框架

--- main/continuum_robot_esp32.c
+++ main/continuum_robot_esp32.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+void start_main_task(void *pvParameters);
 
 void app_main(void)
 {
+    xTaskCreatePinnedToCore(start_main_task, "main_task", 2048, NULL, 5, NULL, 1);
+}
 
+void start_main_task(void *pvParameters)
+{
+    while (1)
+    {
+        ESP_LOGI("main_task", "Hello world!");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
